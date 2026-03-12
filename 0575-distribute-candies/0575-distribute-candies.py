36079ac3e26c73453536be964d52d3631ed9c572
disha1252 <dishagogikar@gmail.com>
2026-03-12 20:06:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,3 +1,7 @@
 class Solution:
-    def distributeCandies(self, candyType):
-        return min(len(set(candyType)), len(candyType)//2)
+    def distributeCandies(self, candyType: List[int]) -> int:
+        length=len(set(candyType))
+        if len(candyType)//2<=length:
+            return len(candyType)//2
+        else:
+            return length
